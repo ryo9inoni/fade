080ef6eabee2a085d411731e60923e3e0d5ec3da
RyotaInoue <ri@nudo.jp>
2021-12-14 17:47:01 +0900

    リファクタリング

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /***
  * @param {String}  type フェードの種類[in, out]
  * @param {Element} element 要素[dom]
- * @param {Number}  duration 変化の時間[transition]
- * @param {String}  easing 変化の速度[transition]
+ * @param {Number}  duration 変化の時間 [css transition-duration: 0000ms]
+ * @param {String}  easing 変化の速度 [css transition-timing-function: easing]
 ***/
 
 /*** example ***
@@ -10,38 +10,40 @@
  * FADE('in', html, 300, 'ease-out');
 ***/
 
-const FADE = (type = '', element = null, duration = 0, easing = '') => {
+const FADE = (type= '', element= null, duration= 0, easing= '') => {
   
   let flag = true;
-  const result = type == 'in' ? 'block' : '0';
+  const valueFirst = type == 'in' ? 'block' : '0';
+  const valueLast = type == 'in' ? '1' : 'none';
 
   switch (type) {
     case 'in':
-      element.style.display = result;
+      element.style.display = valueFirst;
       break;
     case 'out':
-      element.style.transition = duration + 'ms ' + easing;
-      element.style.opacity = result;
+      element.style.transitionDuration = duration+'ms';
+      element.style.transitionTimingFunction = easing;
+      element.style.opacity = valueFirst;
       break;
   }
 
   const tick = () => {
-    
     const property = type == 'in' ? 'display' : 'opacity';
     const style = window.getComputedStyle(element);
     const value = style.getPropertyValue(property);
 
     switch (type) {
       case 'in':
-        if (result == value) {
-          element.style.transition = duration + 'ms ' + easing;
-          element.style.opacity = '1';
+        if (valueFirst == value) {
+          element.style.transitionDuration = duration+'ms';
+          element.style.transitionTimingFunction = easing;
+          element.style.opacity = valueLast;
           flag = false;
         }
         break;
       case 'out':
-        if (result == value) {
-          element.style.display = 'none';
+        if (valueFirst == value) {
+          element.style.display = valueLast;
           flag = false;
         }
         break;
