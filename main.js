217b0b9c3b45a17dc02b212daee59c42c928de1b
RyotaInoue <ri@nudo.jp>
2021-12-14 16:59:39 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+/***
+ * @param {String}  type フェードの種類[in, out]
+ * @param {Element} element 要素[dom]
+ * @param {Number}  duration 変化の時間[transition]
+ * @param {String}  easing 変化の速度[transition]
+***/
+
+/*** example ***
+ * const html = document.querySelector('html');
+ * Fade('in', html, 300, 'ease-out');
+***/
+
+const Fade = (type = '', element = null, duration = 0, easing = '') => {
+  
+  let flag = true;
+  const result = type == 'in' ? 'block' : '0';
+
+  switch (type) {
+    case 'in':
+      element.style.display = result;
+      break;
+    case 'out':
+      element.style.transition = duration + 'ms ' + easing;
+      element.style.opacity = result;
+      break;
+  }
+
+  const tick = () => {
+    
+    const property = type == 'in' ? 'display' : 'opacity';
+    const style = window.getComputedStyle(element);
+    const value = style.getPropertyValue(property);
+
+    switch (type) {
+      case 'in':
+        if (value == result) {
+          element.style.transition = duration + 'ms ' + easing;
+          element.style.opacity = '1';
+          flag = false;
+        }
+        break;
+      case 'out':
+        if (value == result) {
+          element.style.display = 'none';
+          flag = false;
+        }
+        break;
+    }
+
+    if (flag) window.requestAnimationFrame(tick);
+    
+  };
+  tick();
+
+}
