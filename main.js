959ff064b77894c669c79cbe456a7c0c58481281
井上良太 <ryo9inoni@gmail.com>
2022-07-14 17:37:58 +0900

    [update]

--- main.js
+++ main.js
@@ -5,64 +5,64 @@
  * @param {String}  easing 変化の速度 [css transition-timing-function: easing]
  *
  * *** example ***
- * const element = document.querySelector("html");
- * FADE(element, "in", 300, "ease-out");
+ * const element = document.querySelector('html');
+ * FADE(element, 'in', 300, 'ease-out');
 ***/
 
-const FADE = (element = null, type = "in", duration = 300, easing = "ease-out") => {
+const FADE = (element = null, type = 'in', duration = 300, easing = 'ease-out') => {
 
 	// propertyValue
-	const valueFirst = type == "in" ? "block" : "0";
-	const valueLast = type == "in" ? "1" : "none";
-	const property = type == "in" ? "display" : "opacity";
+	const valueFirst = type == 'in' ? 'block' : '0'
+	const valueLast = type == 'in' ? '1' : 'none'
+	const property = type == 'in' ? 'display' : 'opacity'
 
 	// transitionStyle
 	const transition = () => {
-		element.style.transitionDuration = duration+"ms";
-		element.style.transitionTimingFunction = easing;
-		element.addEventListener("transitionend", () => {
-			element.style.transitionDuration = "";
-			element.style.transitionTimingFunction = "";
-		});
+		element.style.transitionDuration = duration+'ms'
+		element.style.transitionTimingFunction = easing
+		element.addEventListener('transitionend', () => {
+			element.style.transitionDuration = ''
+			element.style.transitionTimingFunction = ''
+		})
 	}
 
 	// styleFirst
 	switch (type) {
-		case "in":
-			element.style.display = valueFirst;
-			break;
-		case "out":
-			transition();
-			element.style.opacity = valueFirst;
-			break;
+		case 'in':
+			element.style.display = valueFirst
+			break
+		case 'out':
+			transition()
+			element.style.opacity = valueFirst
+			break
 	}
 
 	// styleを監視
 	const watch = () => {
 
 		// style, propertyValueを取得
-		const style = window.getComputedStyle(element);
-		const value = style.getPropertyValue(property);
+		const style = window.getComputedStyle(element)
+		const value = style.getPropertyValue(property)
 
 		// styleLast
 		switch (type) {
-			case "in":
+			case 'in':
 				if (valueFirst == value) {
-          transition();
-          element.style.opacity = valueLast;
+          transition()
+          element.style.opacity = valueLast
 				}
-				break;
-			case "out":
+				break
+			case 'out':
 				if (valueFirst == value) {
-					element.style.display = valueLast;
+					element.style.display = valueLast
 				}
-				break;
+				break
 		}
 
 		// valueFirstとvalueが同じ値になるまで間、styleを監視
-		if (valueFirst !== value) requestAnimationFrame(watch);
-	};
-	watch();
+		if (valueFirst !== value) requestAnimationFrame(watch)
+	}
+	watch()
 
 }
-export default FADE;
+export default FADE
