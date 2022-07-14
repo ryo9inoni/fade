34e3a32b5b4c7d3aff9cca66402c4e1e3c2d3eaf
井上良太 <ryo9inoni@gmail.com>
2022-07-14 17:32:53 +0900

    [update]

--- main.js
+++ main.js
@@ -3,66 +3,66 @@
  * @param {Element} element 要素[dom]
  * @param {Number}  duration 変化の時間 [css transition-duration: 0000ms]
  * @param {String}  easing 変化の速度 [css transition-timing-function: easing]
- * 
+ *
  * *** example ***
- * const html = document.querySelector('html');
- * FADE('in', html, 300, 'ease-out');
+ * const element = document.querySelector("html");
+ * FADE(element, "in", 300, "ease-out");
 ***/
 
-const FADE = (type = '', element = null, duration = 0, easing = '') => {
-  
-  // propertyValue
-  const valueFirst = type == 'in' ? 'block' : '0';
-  const valueLast = type == 'in' ? '1' : 'none';
-  const property = type == 'in' ? 'display' : 'opacity';
-  
-  // transitionStyle
-  const transition = () => {
-    element.style.transitionDuration = duration+'ms';
-    element.style.transitionTimingFunction = easing;
-  }
+const FADE = (element = null, type = "in", duration = 300, easing = "ease-out") => {
 
-  // styleFirst
-  switch (type) {
-    case 'in':
-      element.style.display = valueFirst;
-      break;
-    case 'out':
-      transition();
-      element.style.opacity = valueFirst;
-      break;
-  }
+	// propertyValue
+	const valueFirst = type == "in" ? "block" : "0";
+	const valueLast = type == "in" ? "1" : "none";
+	const property = type == "in" ? "display" : "opacity";
 
-  //　styleを監視
-  const tick = () => { 
-    
-    // style, propertyValueを取得
-    const style = window.getComputedStyle(element);
-    const value = style.getPropertyValue(property);
-    
-    // styleLast
-    switch (type) {
-      case 'in':
-        if (valueFirst == value) {
+	// transitionStyle
+	const transition = () => {
+		element.style.transitionDuration = duration+"ms";
+		element.style.transitionTimingFunction = easing;
+		element.addEventListener("transitionend", () => {
+			element.style.transitionDuration = "";
+			element.style.transitionTimingFunction = "";
+		});
+	}
+
+	// styleFirst
+	switch (type) {
+		case "in":
+			element.style.display = valueFirst;
+			break;
+		case "out":
+			transition();
+			element.style.opacity = valueFirst;
+			break;
+	}
+
+	// styleを監視
+	const watch = () => {
+
+		// style, propertyValueを取得
+		const style = window.getComputedStyle(element);
+		const value = style.getPropertyValue(property);
+
+		// styleLast
+		switch (type) {
+			case "in":
+				if (valueFirst == value) {
           transition();
           element.style.opacity = valueLast;
-        }
-        break;
-      case 'out':
-        if (valueFirst == value) {
-          element.style.display = valueLast;
-        }
-        break;
-    }
+				}
+				break;
+			case "out":
+				if (valueFirst == value) {
+					element.style.display = valueLast;
+				}
+				break;
+		}
 
-    // valueFirstとvalueが同じ値になるまで間、styleを監視
-    if (valueFirst !== value) {
-      window.requestAnimationFrame(tick);
-    }
-    
-  };
-  tick();
+		// valueFirstとvalueが同じ値になるまで間、styleを監視
+		if (valueFirst !== value) requestAnimationFrame(watch);
+	};
+	watch();
 
 }
-
 export default FADE;
